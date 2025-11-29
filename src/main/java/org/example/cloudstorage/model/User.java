b4e7f71c5ebd735d9633fe875f147836fb1c08be
van0mas <van35russ@gmail.com>
2025-11-30 00:42:20 +0300

    feat: add User entity and JPA

---
+++ src/main/java/org/example/cloudstorage/model/User.java
@@ -0,0 +1,19 @@
+package org.example.cloudstorage.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+}
