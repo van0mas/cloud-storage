9e8a5a413bcdca572299268f339caf4c0a48983c
van0mas <van35russ@gmail.com>
2025-11-30 03:43:32 +0300

    feat: add UserService with repository methods and exceptions

--- src/main/java/org/example/cloudstorage/repository/UserRepository.java
+++ src/main/java/org/example/cloudstorage/repository/UserRepository.java
@@ -3,5 +3,9 @@ package org.example.cloudstorage.repository;
 import org.example.cloudstorage.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
 }
