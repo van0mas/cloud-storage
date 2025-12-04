bed793b8663ab8047f532d201d38f77f0ef4b325
van0mas <van35russ@gmail.com>
2025-12-04 19:56:48 +0300

    feat(security): add dev profile + dev config

--- src/main/java/org/example/cloudstorage/config/DevSecurityConfig.java
+++ src/main/java/org/example/cloudstorage/config/DevSecurityConfig.java
@@ -1,35 +1,25 @@
 package org.example.cloudstorage.config;
 
-import lombok.RequiredArgsConstructor;
-import org.example.cloudstorage.security.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-@Profile("prod")
-public class SecurityConfig {
-
-    private final UserDetailsServiceImpl userDetailsService;
+@Profile("dev")
+public class DevSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer::disable) // CSRF отключён
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll() // открытые эндпоинты
-                        .anyRequest().authenticated() // все остальные защищены
-                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .build();
     }
 
